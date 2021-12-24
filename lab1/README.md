b42f408e2db1ea52439fed62bd5c20998164da1d
fatsoap <fatsoap429@icloud.com>
2021-12-24 20:51:27 +0800

    lab1 done

--- lab1/README.md
+++ lab1/README.md
@@ -5,6 +5,7 @@
 ## Compile c++ file
 
 ```
+// macos
 g++ -g ./lab1.cpp -o ./lab1 -framework OpenGL -framework GLUT
 ```
 
@@ -13,3 +14,44 @@ g++ -g ./lab1.cpp -o ./lab1 -framework OpenGL -framework GLUT
 ```
 ./lab1
 ```
+
+##
+
+1. 畫點(d):
+
+   - size:1
+   - color:紅色
+
+2. 滑鼠左鍵按下時畫
+
+   - 線(l):
+   - size:1
+   - color:綠色
+   - 滑鼠左鍵第一下是起點，第二下是終點
+   - 以向量搭配 x, y 變化量用點畫線
+
+3. 多邊形(p):
+
+   - size:1
+   - color:藍色
+   - 滑鼠左鍵第一下是起點，第二下以後會連線上一個點
+   - 以向量搭配 x, y 變化量用點畫線
+
+4. 圓(o):
+
+   - size:1
+   - color:黃色
+   - 滑鼠左鍵點擊為圓心 放開為半徑
+   - 使用八分畫圓法用點畫圓
+
+5. 清除(c):
+
+   - 隱藏所有圖形
+
+6. 恢復(r):
+
+   - 顯示所有圖形
+
+7. 結束程式(q):
+
+   - 關閉視窗，並且結束程式
--- lab1/lab1.cpp
+++ lab1/lab1.cpp
@@ -2,6 +2,8 @@
 #include <GLUT/glut.h>
 #include <OpenGL/gl.h>
 #include <iostream>
+#include <vector>
+#include <cmath>
 using namespace std;
 
 int window_id = -1;
@@ -10,27 +12,272 @@ int line_buffer[2] = { -1, -1 };
 int polygon_buffer[2] = { -1, -1 };
 int circle_buffer[2] = { -1, -1 };
 
+struct dot {
+	int x;
+	int y;
+	bool show;
+};
+
+struct line {
+	int x1, x2;
+	int y1, y2;
+	bool show;
+};
+
+struct polygon {
+	int x1, x2;
+	int y1, y2;
+	bool show;
+};
+
+struct circle {
+	int x1, x2;
+	int y1, y2;
+	bool show;
+};
+
+vector<dot> dots;
+vector<line> lines;
+vector<polygon> polygons;
+vector<circle> circles;
+
 void display();
 void mouseFunc(int bin, int state, int x, int y);
-void keyboardFunc(unsigned char key, int x, int y);
 void initBuffer();
+void keyboardFunc(unsigned char key, int x, int y);
+void handleDot(int bin, int state, int x, int y);
+void handleLine(int bin, int state, int x, int y);
+void handlePolygon(int bin, int state, int x, int y);
+void handleCircle(int bin, int state, int x, int y);
 void handleClear();
 void handleReset();
+void drawLine(int x1, int y1, int x2, int y2);
+void drawCircle(int x1, int y1, int x2, int y2);
 
+void display() {
+	// set bg color
+	glClearColor(0.0, 0.0, 0.0, 0.0);
+	glClear(GL_COLOR_BUFFER_BIT);
+	glPointSize(1);
+	glColor3f(1.0f, 0.0f, 0.0f);
+	glBegin(GL_POINTS);
+	for (int i = 0; i < dots.size(); i++) {
+		if (!dots[i].show) continue;
+		glVertex2i(dots[i].x, dots[i].y);
+	}
+	glColor3f(0.0f, 1.0f, 0.0f);
+	for (int i = 0; i < lines.size(); i++) {
+		if (!lines[i].show) continue;
+		drawLine(lines[i].x1, lines[i].y1, lines[i].x2, lines[i].y2);
+	}
+	glColor3f(0.0f, 0.0f, 1.0f);
+	for (int i = 0; i < polygons.size(); i++) {
+		if (!polygons[i].show) continue;
+		drawLine(polygons[i].x1, polygons[i].y1, polygons[i].x2, polygons[i].y2);
+	}
+	glColor3f(1.0f, 1.0f, 0.0f);
+	for (int i = 0; i < circles.size(); i++) {
+		if (!circles[i].show) continue;
+		drawCircle(circles[i].x1, circles[i].y1, circles[i].x2, circles[i].y2);
+	}
+	glEnd();
+	glutSwapBuffers();
+}
 
-void display() {};
+void drawCircle(int x1, int y1, int x2, int y2) {
+	int r = sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+	int a = 0;
+	int y = (int)(r * 1.0 / sqrt(2));
+	float d0 = 1.25 - r;
+	float d;
+	int cx = 0, cy = r;
+	while (a <= y) {
+		glVertex2i(x1+a, y1+cy);
+		glVertex2i(x1-a, y1-cy);
+		glVertex2i(x1-a, y1+cy);
+		glVertex2i(x1+a, y1-cy);
+		glVertex2i(x1+cy, y1+a);
+		glVertex2i(x1-cy, y1-a);
+		glVertex2i(x1+cy, y1-a);
+		glVertex2i(x1-cy, y1+a);
+
+		a++;
+		if (d0 <= 0 ){
+			d0 = d0 + 2 * a + 3;
+			cy =cy;
+		}else {
+			d0 = d0 + 2 * (a - cy) + 5;
+			cy = cy - 1;
+		}
+	}
+}
+
+void drawLine(int x1, int y1, int x2, int y2) {
+	if (abs(x1 - x2) >= abs(y1 - y2)) {
+		if (x1 == x2) { // same -> dot
+			glVertex2i(x1, y1);
+		}
+		if (x1 > x2) { //swap
+			int tmp = x1;
+			x1 = x2;
+			x2 = tmp;
+			tmp = y1;
+			y1 = y2;
+			y2 = tmp;
+		}
+		float dir = ((float)(y2)-(float)(y1)) / ((float)(x2)-(float)(x1));
+		for (int j = x1; j <= x2; j++) {
+			int k = (int)((j - x1) * dir) + y1;
+			glVertex2i(j, k);
+		}
+	}
+	else {
+		if (y1 > y2) { //swap
+			int tmp = x1;
+			x1 = x2;
+			x2 = tmp;
+			tmp = y1;
+			y1 = y2;
+			y2 = tmp;
+		}
+		float dir = ((float)(x2)-(float)(x1)) / ((float)(y2)-(float)(y1));
+		for (int j = y1; j <= y2; j++) {
+			int k = (int)((j - y1) * dir) + x1;
+			glVertex2i(k, j);
+		}
+	}
+}
+
+void handleDot(int bin, int state, int x, int y) {
+	if (bin == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		struct dot new_dot;
+		new_dot.x = x;
+		new_dot.y = glutGet(GLUT_WINDOW_HEIGHT) - y;
+		new_dot.show = true;
+		dots.push_back(new_dot);
+		glutPostRedisplay();
+	}
+}
+
+void handleLine(int bin, int state, int x, int y) {
+	if (bin == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		if (line_buffer[0] == -1 && line_buffer[1] == -1) { // first
+			line_buffer[0] = x, line_buffer[1] = glutGet(GLUT_WINDOW_HEIGHT) - y;
+		}
+		else { // second
+			struct line new_line;
+			new_line.x1 = line_buffer[0];
+			new_line.y1 = line_buffer[1];
+			new_line.x2 = x;
+			new_line.y2 = glutGet(GLUT_WINDOW_HEIGHT) - y;
+			new_line.show = true;
+			lines.push_back(new_line);
+			line_buffer[0] = -1, line_buffer[1] = -1;
+			glutPostRedisplay();
+		}
+	}
+}
+
+void handlePolygon(int bin, int state, int x, int y) {
+	if (bin == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		if (polygon_buffer[0] == -1 && polygon_buffer[1] == -1) { // first
+			polygon_buffer[0] = x, polygon_buffer[1] = glutGet(GLUT_WINDOW_HEIGHT) - y;
+		}
+		else { // second
+			struct polygon new_polygon;
+			new_polygon.x1 = polygon_buffer[0];
+			new_polygon.y1 = polygon_buffer[1];
+			new_polygon.x2 = x;
+			new_polygon.y2 = glutGet(GLUT_WINDOW_HEIGHT) - y;
+			new_polygon.show = true;
+			polygons.push_back(new_polygon);
+			polygon_buffer[0] = x, polygon_buffer[1] = glutGet(GLUT_WINDOW_HEIGHT) - y;
+			glutPostRedisplay();
+		}
+	}
+}
+
+void handleCircle(int bin, int state, int x, int y) {
+	if (bin == GLUT_LEFT_BUTTON && state == GLUT_DOWN) {
+		if (circle_buffer[0] == -1 && circle_buffer[1] == -1) { // first
+			circle_buffer[0] = x, circle_buffer[1] = glutGet(GLUT_WINDOW_HEIGHT) - y;
+		}
+	}
+	if (bin == GLUT_LEFT_BUTTON && state == GLUT_UP) {
+		if (circle_buffer[0] != -1 || circle_buffer[1] != -1) { // second
+			struct circle new_circle;
+			new_circle.x1 = circle_buffer[0];
+			new_circle.y1 = circle_buffer[1];
+			new_circle.x2 = x;
+			new_circle.y2 = glutGet(GLUT_WINDOW_HEIGHT) - y;
+			new_circle.show = true;
+			circles.push_back(new_circle);
+			circle_buffer[0] = -1, circle_buffer[1] = -1;
+			glutPostRedisplay();
+		}
+	}
+}
+
 void handleClear() {
+	for (int i = 0; i < dots.size(); i++) {
+		dots[i].show = false;
+	}
+	for (int i = 0; i < lines.size(); i++) {
+		lines[i].show = false;
+	}
+	for (int i = 0; i < polygons.size(); i++) {
+		polygons[i].show = false;
+	}
+	for (int i = 0; i < circles.size(); i++) {
+		circles[i].show = false;
+	}
+	initBuffer();
+	glutPostRedisplay();
 
-};
+}
 
 void handleReset() {
+	for (int i = 0; i < dots.size(); i++) {
+		dots[i].show = true;
+	}
+	for (int i = 0; i < lines.size(); i++) {
+		lines[i].show = true;
+	}
+	for (int i = 0; i < polygons.size(); i++) {
+		polygons[i].show = true;
+	}
+	for (int i = 0; i < circles.size(); i++) {
+		circles[i].show = true;
+	}
+	initBuffer();
+	glutPostRedisplay();
+}
 
-};
+void mouseFunc(int bin, int state, int x, int y) {
+	switch (mode) {
+	case 0:
+		handleDot(bin, state, x, y);
+		break;
+	case 1:
+		handleLine(bin, state, x, y);
+		break;
+	case 2:
+		handlePolygon(bin, state, x, y);
+		break;
+	case 3:
+		handleCircle(bin, state, x, y);
+		break;
+	}
+}
 
-void mouseFunc(int bin, int state, int x, int y) {};
+void initBuffer() {
+	line_buffer[0] = -1, line_buffer[1] = -1;
+	polygon_buffer[0] = -1, polygon_buffer[1] = -1;
+	circle_buffer[0] = -1, circle_buffer[1] = -1;
+}
 
 void keyboardFunc(unsigned char key, int x, int y) {
-    switch (key) {
+	switch (key) {
 	case 'd':
 		if (mode != 0) initBuffer();
 		mode = 0;
@@ -60,12 +307,6 @@ void keyboardFunc(unsigned char key, int x, int y) {
 		}		
 		break;
 	}
-};
-
-void initBuffer() {
-    line_buffer[0] = -1, line_buffer[1] = -1;
-	polygon_buffer[0] = -1, polygon_buffer[1] = -1;
-	circle_buffer[0] = -1, circle_buffer[1] = -1;
 }
 
 int main(int argc, char** argv) {
